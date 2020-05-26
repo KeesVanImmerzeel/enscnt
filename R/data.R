de93a3a6989e96357bddc546bd39c14bd4c4ff7e
KeesVanImmerzeel <idodoes@xs4all.nl>
2020-05-26 14:57:06 +0200

    data.R (with minor changes) moved to R folder

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Sample of English sentences.
+#'
+#' A dataset containing sample sentences from twitter, blogs and newspapers.
+#' @source \url{https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip}
+#'
+#' For constructing this package, 25% of this data is selected with the script
+#' 'data-raw/sampling.r' and written in a file 'data-raw/en.txt'.
+#'
+#' (The original files would be too large to be uploaded on GitHub).
+#'
+#' The lines in 'data-raw/en.txt' are cleaned with the function clean_text()
+#' (as included in this package) and made available as a dataset 'ensentences'.
+#'
+#' @examples
+#' To show the first sentence of the dataset:
+#'
+#' \dontrun{
+#' ensentences[1]
+#' }
+"ensentences"
