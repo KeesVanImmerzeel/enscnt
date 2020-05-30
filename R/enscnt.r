edf293880276fcb055da73a83a73fb21566d6674
KeesVanImmerzeel <idodoes@xs4all.nl>
2020-05-30 18:43:34 +0200

    Minor change in enscnt.r

--- R/enscnt.r
+++ R/enscnt.r
@@ -7,12 +7,16 @@
 #'
 #' @section functions:
 #' Additional functions exported:
+#'
 #' \code{\link{clean_text}}
+#'
 #' \code{\link{word_count}}
-#' \code{\link{word_count}}
-#' \code{\link{word_count}}
-#' \code{\link{word_count}}
-#' \code{\link{word_count}}
+#'
+#' \code{\link{ngrams_table}}
+#'
+#' \code{\link{sampleNlines}}
+#'
+#' \code{\link{plot_ngrams}}
 #'
 #' @docType package
 #' @name enscnt
